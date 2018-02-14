3e0b8aa0abbbf3b32e5df9b4dac4e97327317966
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-14 19:51:30 +0100

    FoodProduct implemented

--- src/main/java/com/codecool/plaza/api/FoodProduct.java
+++ src/main/java/com/codecool/plaza/api/FoodProduct.java
@@ -1,4 +1,32 @@
 package com.codecool.plaza.api;
 
-public class FoodProduct extends Product{
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FoodProduct extends Product {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    private int calories;
+    private Date bestBefore;
+    private Date currentDate = new Date();
+
+    public FoodProduct(long barcode,String manufacturer,String name,int calories,String bestBefore) throws ParseException{
+        super(barcode,manufacturer,name);
+        this.calories = calories;
+        this.bestBefore = dateFormat.parse(bestBefore);
+    }
+
+    public boolean isStillConsumeable() {
+        return (currentDate.after(bestBefore));
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
