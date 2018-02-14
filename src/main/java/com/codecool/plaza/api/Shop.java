6729fb3e0c398ac30cb557120e0dec99c971899d
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-14 17:22:31 +0100

    Shop interface implemented

--- src/main/java/com/codecool/plaza/api/Shop.java
+++ src/main/java/com/codecool/plaza/api/Shop.java
@@ -1,4 +1,19 @@
 package com.codecool.plaza.api;
 
+import java.util.List;
+
 public interface Shop {
+    String getName();
+    String getOwner();
+    boolean isOpen();
+    void open();
+    void close();
+    Product findByName(String name) throws NoSuchShopException,ShopIsClosedException;
+    boolean hasProduct(long barcode) throws ShopIsClosedException;
+    void addNewProduct(Product product,int quantity,float price) throws ProductAlreadyExistsException,ShopIsClosedException;
+    void addProduct(long barcode,int quantity) throws NoSuchProductException,ShopIsClosedException;
+    Product buyProduct(long barcode) throws NoSuchProductException,OutOfStockException,ShopIsClosedException;
+    List<Product> buyProducts(long barcode,int quantity) throws NoSuchProductException,OutOfStockException,ShopIsClosedException;
+    String toString();
+
 }
