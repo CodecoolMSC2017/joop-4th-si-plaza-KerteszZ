51f96596dd31e151727e1aa14e87407892b24322
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-14 20:28:30 +0100

    FindShopByName method implemented Exceptions added to methods

--- src/main/java/com/codecool/plaza/api/PlazaImpl.java
+++ src/main/java/com/codecool/plaza/api/PlazaImpl.java
@@ -14,22 +14,45 @@ public class PlazaImpl implements Plaza {
 
     @Override
     public List<Shop> getShops() throws PlazaIsClosedException {
+        if (!isOpen()) {
+            throw new PlazaIsClosedException();
+        }
         return shops;
     }
 
     @Override
     public void addShop(Shop shop) throws ShopAlreadyExistsException, PlazaIsClosedException {
+        if (!isOpen()) {
+            throw new PlazaIsClosedException();
+        }
+        if (shops.contains(shop)) {
+            throw new ShopAlreadyExistsException();
+        }
         shops.add(shop);
     }
 
     @Override
     public void removeShop(Shop shop) throws NoSuchShopException, PlazaIsClosedException {
+        if (!isOpen()) {
+            throw new PlazaIsClosedException();
+        }
+        if (!shops.contains(shop)) {
+            throw new NoSuchShopException();
+        }
         shops.remove(shop);
     }
 
     @Override
-    public Shop findShopByName(String name) {
-        return null;
+    public Shop findShopByName(String name) throws NoSuchShopException ,PlazaIsClosedException{
+        if (!isOpen()) {
+            throw new PlazaIsClosedException();
+        }
+        for (int i = 0;i < shops.size();i++) {
+            if (shops.get(i).getName().equals(name)) {
+                return shops.get(i);
+            }
+        }
+        throw new NoSuchShopException();
     }
 
     @Override
