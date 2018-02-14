003cd614c9dbfd906d87487e8447e04f4e4c5a29
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-14 19:56:52 +0100

    ClothingProduct implemented

--- src/main/java/com/codecool/plaza/api/ClothingProduct.java
+++ src/main/java/com/codecool/plaza/api/ClothingProduct.java
@@ -1,4 +1,25 @@
 package com.codecool.plaza.api;
 
 public class ClothingProduct extends Product{
+    private String material;
+    private String type;
+
+    public ClothingProduct(long barcode, String manufacturer, String name,String material,String type) {
+        super(barcode, manufacturer, name);
+        this.material = material;
+        this.type = type;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
